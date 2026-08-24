6d8ded4c38bd7e11fc86b81581fca89f7c753db8
Gour S Patar <goursundarpatar2@gmail.com>
2026-08-24 22:11:36 +0530

    Move database credentials to environment variables

--- app/database.py
+++ app/database.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
-DATABASE_URL = "postgresql://postgres:gour%405862@localhost:5432/product_catalog"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 engine = create_engine(DATABASE_URL)
